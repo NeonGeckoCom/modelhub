5cb1b510bce8e9bd5d68b39b1b48ba1a977d97a1
jarbasai <jarbasai@mailfence.com>
2021-11-25 20:08:25 +0000

    manifest + model (down)loading

---
+++ neon_modelhub/__init__.py
@@ -0,0 +1,30 @@
+import os
+from os.path import join, isfile
+
+import joblib
+import requests
+import xdg.BaseDirectory
+
+from neon_modelhub.manifest import MANIFEST
+
+MODELS_DIR = join(xdg.BaseDirectory.xdg_cache_home, "ModelZoo")
+
+
+def load_model(model_id):
+    subfolder = model_id.split("_")[0]
+    models_dir = join(MODELS_DIR, subfolder)
+    model_path = join(models_dir, model_id + ".pkl")
+    if not isfile(model_path):
+        download_model(model_id)
+    return joblib.load(model_path)
+
+
+def download_model(model_id):
+    url = MANIFEST[model_id]
+    subfolder = model_id.split("_")[0]
+    models_dir = join(MODELS_DIR, subfolder)
+    os.makedirs(models_dir, exist_ok=True)
+    model_path = join(models_dir, model_id + ".pkl")
+    r = requests.get(url)
+    with open(model_path, "wb") as f:
+        f.write(r.content)
---
+++ neon_modelhub/manifest.py
@@ -0,0 +1,74 @@
+import requests
+from json_database import JsonStorageXDG
+
+MANIFEST = JsonStorageXDG("manifest", subfolder="ModelZoo")
+MANIFEST_URL = "https://github.com/NeonJarbas/modelhub/raw/models/models/manifest.json"
+
+
+def populate_manifest():
+    # download manifest file from server
+    try:
+        r = requests.get(MANIFEST_URL).json()
+        MANIFEST.update(r)
+        MANIFEST.store()
+    except:
+        # model urls, used only if manifest does not yet exist
+        # manually maintained list !
+        DEFAULT_MANIFEST = {
+            'nltk_brown_brill_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_brown_brill_tagger.pkl',
+            'nltk_brown_clf_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_brown_clf_tagger.pkl',
+            'nltk_brown_dtree_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_brown_dtree_tagger.pkl',
+            'nltk_brown_maxent_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_brown_maxent_tagger.pkl',
+            'nltk_brown_ngram_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_brown_ngram_tagger.pkl',
+            'nltk_brown_perceptron_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_brown_perceptron_tagger.pkl',
+            'nltk_brown_tnt_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_brown_tnt_tagger.pkl',
+            'nltk_cess_cat_brill_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_cess_cat_brill_tagger.pkl',
+            'nltk_cess_cat_clf_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_cess_cat_clf_tagger.pkl',
+            'nltk_cess_cat_ngram_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_cess_cat_ngram_tagger.pkl',
+            'nltk_cess_cat_udep_brill_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_cess_cat_udep_brill_tagger.pkl',
+            'nltk_cess_cat_udep_ngram_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_cess_cat_udep_ngram_tagger.pkl',
+            'nltk_cess_cat_udep_perceptron_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_cess_cat_udep_perceptron_tagger.pkl',
+            'nltk_cess_esp_brill_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_cess_esp_brill_tagger.pkl',
+            'nltk_cess_esp_clf_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_cess_esp_clf_tagger.pkl',
+            'nltk_cess_esp_ngram_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_cess_esp_ngram_tagger.pkl',
+            'nltk_cess_esp_perceptron_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_cess_esp_perceptron_tagger.pkl',
+            'nltk_dtree_gender_clf': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_dtree_gender_clf.pkl',
+            'nltk_features': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_features.pkl',
+            'nltk_floresta_brill_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_floresta_brill_tagger.pkl',
+            'nltk_floresta_clf_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_floresta_clf_tagger.pkl',
+            'nltk_floresta_macmorpho_brill_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_floresta_macmorpho_brill_tagger.pkl',
+            'nltk_floresta_macmorpho_ngram_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_floresta_macmorpho_ngram_tagger.pkl',
+            'nltk_floresta_macmorpho_perceptron_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_floresta_macmorpho_perceptron_tagger.pkl',
+            'nltk_floresta_ngram_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_floresta_ngram_tagger.pkl',
+            'nltk_floresta_perceptron_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_floresta_perceptron_tagger.pkl',
+            'nltk_floresta_tnt_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_floresta_tnt_tagger.pkl',
+            'nltk_macmorpho_brill_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_macmorpho_brill_tagger.pkl',
+            'nltk_macmorpho_clf_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_macmorpho_clf_tagger.pkl',
+            'nltk_macmorpho_ngram_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_macmorpho_ngram_tagger.pkl',
+            'nltk_macmorpho_perceptron_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_macmorpho_perceptron_tagger.pkl',
+            'nltk_macmorpho_udep_brill_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_macmorpho_udep_brill_tagger.pkl',
+            'nltk_macmorpho_udep_clf_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_macmorpho_udep_clf_tagger.pkl',
+            'nltk_macmorpho_udep_ngram_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_macmorpho_udep_ngram_tagger.pkl',
+            'nltk_macmorpho_udep_perceptron_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_macmorpho_udep_perceptron_tagger.pkl',
+            'nltk_maxent_gender_clf': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_maxent_gender_clf.pkl',
+            'nltk_nb_gender_clf': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_nb_gender_clf.pkl',
+            'nltk_nilc_brill_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_nilc_brill_tagger.pkl',
+            'nltk_nilc_clf_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_nilc_clf_tagger.pkl',
+            'nltk_nilc_dtree_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_nilc_dtree_tagger.pkl',
+            'nltk_nilc_ngram_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_nilc_ngram_tagger.pkl',
+            'nltk_nilc_perceptron_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_nilc_perceptron_tagger.pkl',
+            'nltk_onto5_clf_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_onto5_clf_tagger.pkl',
+            'nltk_onto5_ngram_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_onto5_ngram_tagger.pkl',
+            'nltk_onto5_perceptron_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_onto5_perceptron_tagger.pkl',
+            'nltk_treebank_brill_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_treebank_brill_tagger.pkl',
+            'nltk_treebank_clf_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_treebank_clf_tagger.pkl',
+            'nltk_treebank_hmm_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_treebank_hmm_tagger.pkl',
+            'nltk_treebank_maxent_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_treebank_maxent_tagger.pkl',
+            'nltk_treebank_ngram_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_treebank_ngram_tagger.pkl',
+            'nltk_treebank_perceptron_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_treebank_perceptron_tagger.pkl',
+            'nltk_treebank_tnt_tagger': 'https://github.com/NeonJarbas/modelhub/raw/models/models/nltk/nltk_treebank_tnt_tagger.pkl'}
+        if not MANIFEST:
+            MANIFEST.update(DEFAULT_MANIFEST)
+
+
+populate_manifest()
