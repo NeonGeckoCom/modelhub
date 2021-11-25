5d7b49a3687e065a5170f1168e3e53a5d2b52e76
jarbasai <jarbasai@mailfence.com>
2021-11-25 01:28:40 +0000

    nilc

---
+++ train/nltk/postag/brill_tagger_nilc.py
@@ -0,0 +1,47 @@
+import random
+
+import joblib
+import nltk
+from json_database import JsonStorageXDG
+
+import biblioteca
+from biblioteca.corpora.external import NILC
+
+db = JsonStorageXDG("nltk_nilc_brill_tagger", subfolder="ModelZoo/nltk")
+MODEL_META = {
+    "corpus": "NILC_taggers",
+    "corpus_homepage": "http://www.nilc.icmc.usp.br/nilc/tools/nilctaggers.html",
+    "model_id": "nltk_nilc_brill_tagger",
+    "tagset": "NILC",
+    "tagset_homepage": "http://www.nilc.icmc.usp.br/nilc/download/tagsetcompleto.doc",
+    "lang": "pt-br",
+    "algo": "nltk.brill.fntbl37",
+    "backoff_taggers": ["AffixTagger", "UnigramTagger", "BigramTagger",
+                        "TrigramTagger"],
+    "required_packages": ["nltk"]
+}
+
+db.update(MODEL_META)
+db.store()
+model_path = db.path.replace(".json", ".pkl")
+
+biblioteca.download("NILC_taggers")
+nilc = NILC()
+
+data = [s for s in nilc.tagged_sentences()]
+random.shuffle(data)
+cutoff = int(len(data) * 0.9)
+train_data = data[:cutoff]
+test_data = data[cutoff:]
+
+ngram_tagger = joblib.load(model_path.replace("brill", "ngram"))
+
+tagger = nltk.BrillTaggerTrainer(ngram_tagger, nltk.brill.fntbl37())
+tagger = tagger.train(train_data)
+
+a = tagger.evaluate(test_data)
+
+print("Accuracy of Brill tagger : ", a)  # 0.877122686510208
+db["accuracy"] = a
+db.store()
+joblib.dump(tagger, model_path)
---
+++ train/nltk/postag/clf_tagger_nilc.py
@@ -0,0 +1,43 @@
+import random
+
+import joblib
+from json_database import JsonStorageXDG
+from nltk.tag.sequential import ClassifierBasedPOSTagger
+
+import biblioteca
+from biblioteca.corpora.external import NILC
+
+db = JsonStorageXDG("nltk_nilc_clf_tagger", subfolder="ModelZoo/nltk")
+MODEL_META = {
+    "corpus": "NILC_taggers",
+    "corpus_homepage": "http://www.nilc.icmc.usp.br/nilc/tools/nilctaggers.html",
+    "model_id": "nltk_nilc_clf_tagger",
+    "tagset": "NILC",
+    "tagset_homepage": "http://www.nilc.icmc.usp.br/nilc/download/tagsetcompleto.doc",
+    "lang": "pt-br",
+    "algo": "ClassifierBasedPOSTagger",
+    "backoff_taggers": ["AffixTagger", "UnigramTagger", "BigramTagger",
+                        "TrigramTagger"],
+    "required_packages": ["nltk"]
+}
+db.update(MODEL_META)
+db.store()
+model_path = db.path.replace(".json", ".pkl")
+
+biblioteca.download("NILC_taggers")
+nilc = NILC()
+
+data = [s for s in nilc.tagged_sentences()]
+random.shuffle(data)
+cutoff = int(len(data) * 0.9)
+train_data = data[:cutoff]
+test_data = data[cutoff:]
+
+tagger = ClassifierBasedPOSTagger(train=train_data)
+
+a = tagger.evaluate(test_data)
+
+print("Accuracy:", a)  # 0.8753341915041093
+db["accuracy"] = a
+db.store()
+joblib.dump(tagger, model_path)
