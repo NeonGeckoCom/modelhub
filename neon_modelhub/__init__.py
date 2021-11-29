2fa8bd0f3be112e221ea2778c7ba6d2a3354a558
jarbasai <jarbasai@mailfence.com>
2021-11-29 01:06:18 +0000

    fix double file ext

--- neon_modelhub/__init__.py
+++ neon_modelhub/__init__.py
@@ -14,7 +14,9 @@ def load_model(model_id):
     # model_id is {framework}_{dataset}_{algorithm}_{task}
     subfolder = model_id.split("_")[0]
     models_dir = join(MODELS_DIR, subfolder)
-    model_path = join(models_dir, model_id + ".pkl")
+    model_path = join(models_dir, model_id)
+    if not model_path.endswith(".pkl"):
+        model_path += ".pkl"
     if not isfile(model_path):
         download_model(model_id)
     return joblib.load(model_path)
