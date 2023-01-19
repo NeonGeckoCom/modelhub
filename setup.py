3b0234610f671140ac2d12fd4cb46297626441cc
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2023-01-19 10:10:52 -0800

    Update to include all submodules (#2)
    Closes #1

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='neon_classic_modelhub',
-    version='0.0.1a2',
-    packages=['neon_classic_modelhub',
-              'neon_classic_modelhub.features'],
+    version='0.0.1a3',
+    packages=find_packages(),
     license='',
     author='jarbasai',
     author_email='',
